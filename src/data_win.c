9e466131f8bc5fa7a7b00f1f75202bddb98c8b3e
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-06-16 17:57:28 -0300

    Only attempt to detect GM 2024.14+ on the AGRP if the current detected version is GM 2024.13+
    Fixes Underfavela failing to parse because for SOME REASON the path (which most of the time is just padding?) is a non-zero value where the path should be

--- src/data_win.c
+++ src/data_win.c
@@ -664,21 +664,23 @@ static void parseAGRP(BinaryReader* reader, DataWin* dw) {
     // To detect it, we'll check if the difference between two pointers is 8 (two int32)
     // We CAN'T figure out if there aren't at least two AudioGroups, but for any meaningful purposes any game that has external AudioGroups WILL have
     // at least two entries, one for the default AudioGroup and another for the external AudioGroup
-    if (count >= 2) {
-        uint32_t diff = ptrs[1] - ptrs[0];
+    if (DataWin_isVersionAtLeast(dw, 2024, 13, 0, 0)) {
+        if (count >= 2) {
+            uint32_t diff = ptrs[1] - ptrs[0];
 
-        if (diff >= 8) {
-            DataWin_bumpVersionTo(dw, 2024, 14, 0, 0);
-        }
-    } else if (count == 1) {
-        // If there's only one entry, we CAN'T figure out easily based on the pointer diffs
-        // But here's the trick: We can read it twice, if the path is null for the FIRST audiogroup, then it is NOT 2024.14
-        BinaryReader_seek(reader, ptrs[0]);
-        const char* name = readStringPtr(reader, dw);
-        const char* path = readStringPtr(reader, dw);
+            if (diff >= 8) {
+                DataWin_bumpVersionTo(dw, 2024, 14, 0, 0);
+            }
+        } else if (count == 1) {
+            // If there's only one entry, we CAN'T figure out easily based on the pointer diffs
+            // But here's the trick: We can read it twice, if the path is null for the FIRST audiogroup, then it is NOT 2024.14
+            BinaryReader_seek(reader, ptrs[0]);
+            const char* name = readStringPtr(reader, dw);
+            const char* path = readStringPtr(reader, dw);
 
-        if (strcmp(name, "audiogroup_default") == 0 && path != nullptr) {
-            DataWin_bumpVersionTo(dw, 2024, 14, 0, 0);
+            if (strcmp(name, "audiogroup_default") == 0 && path != nullptr) {
+                DataWin_bumpVersionTo(dw, 2024, 14, 0, 0);
+            }
         }
     }
 
